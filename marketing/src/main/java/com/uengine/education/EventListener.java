dbf099a189c8c6fb40004419763f8d3563dfde4c
jyjang <jyjang@uengine.org>
2018-11-22 10:18:40 +0900

    no message

---
+++ marketing/src/main/java/com/uengine/education/EventListener.java
@@ -0,0 +1,23 @@
+package com.uengine.education;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventListener {
+
+    /** version 1. callback version **/
+    @StreamListener(Streams.INPUT)
+    @JsonDeserialize(as = ClazzDayRegistered.class)
+    public void handleClazzDay(@Payload ClazzDayRegistered clazzDayRegistered) {
+
+        System.out.println("<이메일 발송>");
+        System.out.println("제목: " + clazzDayRegistered.getInstructorName() +" 강사의 OOO강의 [마감임박]");
+
+
+    }
+
+}
